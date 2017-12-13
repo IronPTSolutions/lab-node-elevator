a72e7670f9c00ee9b6892262ecc3d46b8f7960f6
Carlos del Prado <carlos.delprado@11paths.com>
2017-12-13 23:22:51 +0100

    feat(elevator): impl call

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,12 +1,17 @@
+const Person = require('./person.js');
+
 const UPDATE_INTERVAL_MILLIS = 1000;
 
 class Elevator {
   constructor(){
     this.floor = 0;
     this.MAXFLOOR = 10;
-    this.requests = [];
     this.direction = "up";
     this.updateIntervalId = undefined;
+
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];    
   }
 
   start() {
@@ -24,6 +29,7 @@ class Elevator {
   update() {
     this.log();
   }
+  
   _passengersEnter() { }
   _passengersLeave() { }
 
@@ -41,7 +47,12 @@ class Elevator {
     }
   }
 
-  call() { }
+  call(person) {
+    if (person instanceof Person) {
+      this.waitingList.push(person);
+      this.requests.push(person.originFloor);
+    }
+  }
 
   log() {
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
